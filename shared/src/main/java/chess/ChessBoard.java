06082a8247d9068a6842d41e49e654318f9e90f9
Fred Probst <github@fsd.probst.org>
2026-09-09 17:15:55 -0600

    adds method to print out board

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,8 +7,8 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
-    private static int BOARD_HEIGHT = 8;
-    private static int BOARD_WIDTH = 8;
+    private static final int BOARD_HEIGHT = 8;
+    private static final int BOARD_WIDTH = 8;
     private ChessPiece myBoard[][] = new ChessPiece[BOARD_HEIGHT][BOARD_WIDTH];
 
     public ChessBoard() {
@@ -24,7 +24,7 @@ public class ChessBoard {
     public void addPiece(ChessPosition position, ChessPiece piece) {
         if (position.getColumn()-1 >= 0 && position.getColumn()-1 <= BOARD_WIDTH - 1
             && position.getRow()-1 >= 0 && position.getRow()-1 <= BOARD_HEIGHT - 1) {
-            if (myBoard[position.getRow()-1][position.getColumn()-1] != null){
+            if (myBoard[position.getRow()-1][position.getColumn()-1] == null){
                 myBoard[position.getRow()-1][position.getColumn()-1] = piece;
             }
         }
@@ -47,5 +47,19 @@ public class ChessBoard {
      */
     public void resetBoard() {
         throw new RuntimeException("Not implemented");
+    }
+
+    public void printBoard() {
+        for (int row = 0; row < BOARD_HEIGHT; row++) {
+            System.out.print("|");
+            for (int col = 0; col < BOARD_WIDTH; col++) {
+                if (myBoard[row][col] == null) {
+                    System.out.print(" |");
+                } else {
+                    System.out.print("x|");
+                }
+            }
+            System.out.print("\n");
+        }
     }
 }
