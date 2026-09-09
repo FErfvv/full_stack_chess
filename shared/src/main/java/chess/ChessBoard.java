f192c284f1f5d9d364ff3cd47df322d5874d957e
Fred Probst <github@fsd.probst.org>
2026-09-09 16:47:26 -0600

    fixes bug with incorrect variable name

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -19,7 +19,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        board[position.getRow()-1][position.getColumn()-1] = piece;
+        myBoard[position.getRow()-1][position.getColumn()-1] = piece;
     }
 
     /**
--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -29,7 +29,6 @@ public class ChessPosition {
      * 1 codes for the left column
      */
     public int getColumn() {
-
         return col;
     }
 }
