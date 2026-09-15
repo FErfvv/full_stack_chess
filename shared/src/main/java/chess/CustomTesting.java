b69820b5fea4893b9e6ccb8258dd6afe03fdf50b
Fred Probst <github@fsd.probst.org>
2026-09-15 13:12:17 -0600

    adds tests for move function

--- shared/src/main/java/chess/CustomTesting.java
+++ shared/src/main/java/chess/CustomTesting.java
@@ -3,9 +3,12 @@ package chess;
 public class CustomTesting {
     public static void main(String[] args) {
         ChessBoard myBoard = new ChessBoard();
+
+        ChessPiece myRook = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+        ChessPosition myRookPosition = new ChessPosition(3,3);
+        myBoard.addPiece(myRookPosition,myRook);
         myBoard.toString();
-        myBoard.resetBoard();
-        System.out.print("\n");
+        myRook.pieceMoves(myBoard,myRookPosition);
         myBoard.toString();
     }
 }
