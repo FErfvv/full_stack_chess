2343cfa759dfc74f3cd3f28dbd8d074c393d0d93
Fred Probst <github@fsd.probst.org>
2026-09-25 14:47:50 -0600

    fixes toString() to be more readable

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -67,6 +67,6 @@ public class ChessMove {
                 "startPosition=" + startPosition +
                 ", endPosition=" + endPosition +
                 ", promotionPiece=" + promotionPiece +
-                '}';
+                "}\n";
     }
 }
